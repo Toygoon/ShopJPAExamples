31fd4b9d21b10524dc2012f414b8ef84e1da5ab2
Toygoon <toygoon@gmail.com>
2023-02-06 16:37:10 +0900

    [Edit] createItemTest에서 상품 여러 개를 추가하도록 수정, 상품을 조회하는 findItemByNameTest 메서드 추가

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -8,26 +8,46 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Random;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.yml")
 class ItemRepositoryTest {
+    // 생성할 Item 개수
+    private static final int TEST_ITEMS = 10;
+
     @Autowired
     private ItemRepository itemRepository;
 
     @Test
     @DisplayName("상품 저장 테스트")
     public void createItemTest() {
-        Item item = new Item();
+        Item item = null;
 
-        item.setName("테스트 상품");
-        item.setPrice(10000);
-        item.setDetail("테스트 상품 상세 설명");
-        item.setSold(true);
-        item.setStockCount(100);
-        item.setRegDate(LocalDateTime.now());
-        item.setUpdatedAt(LocalDateTime.now());
+        for (int i = 1; i <= TEST_ITEMS; i++) {
+            item = new Item();
 
-        System.out.println(itemRepository.save(item));
+            item.setName("테스트 상품 " + i);
+            item.setPrice(10000 + i);
+            item.setDetail("테스트 상품 상세 설명 " + i);
+            item.setSold(new Random().nextBoolean());
+            item.setStockCount(100 + i);
+            item.setRegDate(LocalDateTime.now());
+            item.setUpdatedAt(LocalDateTime.now());
+
+            itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("상품 조회 테스트")
+    public void findItemByNameTest() {
+        this.createItemTest();
+        List<Item> itemList = itemRepository.findItemByName("테스트 상품 " + (new Random().nextInt() % TEST_ITEMS + 1));
+
+        for (Item item : itemList)
+            System.out.println(item);
+
     }
 }
