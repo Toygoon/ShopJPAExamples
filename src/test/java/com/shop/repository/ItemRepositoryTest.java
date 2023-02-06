54f9cd7b080eb1d74fb4dd98993c63b7d2e24399
Toygoon <toygoon@gmail.com>
2023-02-06 16:22:19 +0900

    [Add] ItemRepository 동작 확인을 위한 테스트 클래스 생성

---
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.shop.repository;
+
+import com.shop.entity.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.yml")
+class ItemRepositoryTest {
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Test
+    @DisplayName("상품 저장 테스트")
+    public void createItemTest() {
+        Item item = new Item();
+
+        item.setName("테스트 상품");
+        item.setPrice(10000);
+        item.setDetail("테스트 상품 상세 설명");
+        item.setSold(true);
+        item.setStockCount(100);
+        item.setRegDate(LocalDateTime.MIN);
+        item.setUpdatedAt(LocalDateTime.now());
+
+        System.out.println(itemRepository.save(item).toString());
+    }
+}
