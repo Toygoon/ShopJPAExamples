d8cb26242b70de1595ae65bc63044a8282cc1128
Toygoon <toygoon@gmail.com>
2023-02-06 16:38:05 +0900

    [Add] Item 이름 검색 findItemByName 추가, 이름이나 설명으로 검색하는 findItemByNameOrDetail 추가

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -3,5 +3,17 @@ package com.shop.repository;
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ItemRepository
+ * Item Entity를 JPA가 DB 통신하기 위한 추상 클래스
+ */
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    // 상품 이름으로 객체를 찾는 메서드
+    List<Item> findItemByName(String name);
+
+    // 상품 이름이나 설명으로 객체를 찾는 메서드
+    List<Item> findItemByNameOrDetail(String name, String detail);
 }
