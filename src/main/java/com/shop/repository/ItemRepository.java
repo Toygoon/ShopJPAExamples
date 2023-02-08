ba8b729f7631a18a3c16644253d0031cc79b270a
Toygoon <toygoon@gmail.com>
2023-02-08 14:48:41 +0900

    [Add] Query 문을 이용한 findItemByDetail 메서드 추가

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.shop.repository;
 
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,4 +24,8 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     // 파라미터로 전달된 가격보다 더 저렴한 상품을 찾고, 내림차순으로 정렬하는 메서드
     List<Item> findItemByPriceLessThanOrderByPriceDesc(int price);
+
+    // Query 문을 이용하여 상품 설명으로 객체를 찾는 메서드
+    @Query("SELECT name FROM Item WHERE detail LIKE %:detail% ORDER BY price DESC")
+    List<Item> findItemByDetail(@Param("detail") String detail);
 }
